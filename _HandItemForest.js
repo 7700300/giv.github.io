eda2a297861c1dad3469aaf5155333b278593ee8
7700300 <66745487+7700300@users.noreply.github.com>
2022-03-19 23:40:00 +0300

    Add files via upload

--- _HandItemForest.js
+++ _HandItemForest.js
@@ -1,6 +1,6 @@
 io_parm=new Array();
-io_parm[0]="<a href=javascript:void(0); onclick=\"alert('Not&nbsp;Found');R(2);\" title=\"Надеть Кирку\"><img border=0 src=http://apeha.ru/img/lic/25s.gif width=16 height=16 style=\"background-color:white;\"></a>&nbsp;";
-io_parm[1]="<a href=javascript:void(0); onclick=\"alert('Not&nbsp;Found');R(2);\" title=\"Надеть Топор\"><img border=0 src=http://apeha.ru/img/lic/24s.gif width=16 height=16 style=\"background-color:white;\"></a>";
+io_parm[0]="<a href=javascript:void(0); onclick=\"alert('Not&nbsp;Found');R(2);\" title=\"Надеть Кирку\"><img border=0 src=https://5kings.ru/resources/upload/1_2404.gif width=16 height=16 style=\"background-color:white;\"></a>&nbsp;";
+io_parm[1]="<a href=javascript:void(0); onclick=\"alert('Not&nbsp;Found');R(2);\" title=\"Надеть Топор\"><img border=0 src=https://5kings.ru/resources/upload/1_2403.gif width=16 height=16 style=\"background-color:white;\"></a>";
 
 function actUnWear(i) {
 var element=document.getElementsByTagName("head")[0];
@@ -46,7 +46,7 @@ if(document.getElementsByTagName("td")[i].style.padding) {
 if(document.getElementsByTagName("td")[i].getElementsByTagName("img")[0].title=="Кирка рудокопа") {
 if(document.getElementsByTagName("td")[i+2].getElementsByTagName("input").length) {
 io_parm[0]=document.getElementsByTagName("td")[i+2].getElementsByTagName("input")[0].value;
-io_parm[0]="<a href=javascript:void(0); onclick=\"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[0]+"';RTitle='Кирка рудокопа';R(2);\" title='Надеть Кирку"+i+"' id='hif_2'><img border=0 src=\"http://apeha.ru/img/lic/25s.gif\" "
+io_parm[0]="<a href=javascript:void(0); onclick=\"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[0]+"';RTitle='Кирка рудокопа';R(2);\" title='Надеть Кирку"+i+"' id='hif_2'><img border=0 src=\"https://5kings.ru/resources/upload/1_2404.gif\" "
 +"onload=\""
 +"if(RTitle=='Кирка рудокопа' && RHandUp==1){"
 +"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[0]+"';RTitle='Кирка рудокопа';R(2);"
@@ -60,7 +60,7 @@ if(document.getElementsByTagName("td")[i].style.padding) {
 if(document.getElementsByTagName("td")[i].getElementsByTagName("img")[0].title=="Топор лесоруба") {
 if(document.getElementsByTagName("td")[i+2].getElementsByTagName("input").length) {
 io_parm[1]=document.getElementsByTagName("td")[i+2].getElementsByTagName("input")[0].value;
-io_parm[1]="<a href=javascript:void(0); onclick=\"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[1]+"';RTitle='Топор лесоруба';R(2);\" title='Надеть Топор"+i+"' id='hif_3'><img border=0 src=\"http://apeha.ru/img/lic/24s.gif\" "
+io_parm[1]="<a href=javascript:void(0); onclick=\"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[1]+"';RTitle='Топор лесоруба';R(2);\" title='Надеть Топор"+i+"' id='hif_3'><img border=0 src=\"https://5kings.ru/resources/upload/1_2403.gif\" "
 +"onload=\""
 +"if(RTitle=='Топор лесоруба' && RHandUp==1){"
 +"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[1]+"';RTitle='Топор лесоруба';R(2);"
@@ -77,7 +77,7 @@ var breq=document.getElementsByTagName("td")[i+4].innerHTML+"dot";
 var reg=new RegExp("количество: 1dot","g");
 if(reg.test(breq)) {
 io_parm[0]=document.getElementsByTagName("td")[i+2].getElementsByTagName("input")[0].value;
-io_parm[0]="<a href=javascript:void(0); onclick=\"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[0]+"';RTitle='Кирка рудокопа';R(2);\" title='Надеть Кирку1' id='hif_2'><img border=0 src=\"http://apeha.ru/img/lic/25s.gif\" "
+io_parm[0]="<a href=javascript:void(0); onclick=\"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[0]+"';RTitle='Кирка рудокопа';R(2);\" title='Надеть Кирку1' id='hif_2'><img border=0 src=\"https://5kings.ru/resources/upload/1_2404.gif\" "
 +"onload=\""
 +"if(RTitle=='Кирка рудокопа' && RHandUp==1){"
 +"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[0]+"';RTitle='Кирка рудокопа';R(2);"
@@ -94,7 +94,7 @@ var breq=document.getElementsByTagName("td")[i+4].innerHTML+"dot";
 var reg=new RegExp("количество: 1dot","g");
 if(reg.test(breq)) {
 io_parm[1]=document.getElementsByTagName("td")[i+2].getElementsByTagName("input")[0].value;
-io_parm[1]="<a href=javascript:void(0); onclick=\"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[1]+"';RTitle='Топор лесоруба';R(2);\" title='Надеть Топор1' id='hif_3'><img border=0 src=\"http://apeha.ru/img/lic/24s.gif\" "
+io_parm[1]="<a href=javascript:void(0); onclick=\"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[1]+"';RTitle='Топор лесоруба';R(2);\" title='Надеть Топор1' id='hif_3'><img border=0 src=\"https://5kings.ru/resources/upload/1_2403.gif\" "
 +"onload=\""
 +"if(RTitle=='Топор лесоруба' && RHandUp==1){"
 +"frames['handforest'].location.href='bag_type_17.html?actUser-Wear="+io_parm[1]+"';RTitle='Топор лесоруба';R(2);"
